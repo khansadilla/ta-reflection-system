864472d736df19a294bb18679380396d63f5a582
khansadilla <117127484+khansadilla@users.noreply.github.com>
2026-04-05 15:30:26 +0700

    change llm to gemini

--- llm/model.py
+++ llm/model.py
@@ -1,17 +1,23 @@
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
+'''
 llm = ChatOpenAI(
     model="gpt-4o-mini", 
     temperature=0.7
 )
-'''llm = ChatGroq(
+llm = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.7,
     frequency_penalty=0.6,
     request_timeout=20
 )'''
 
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash", 
+    temperature=0.7
+)
 
 
 llm_judge = ChatGroq(
