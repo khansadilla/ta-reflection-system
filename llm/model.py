c2c6e977322c79b86213a52a702d4dbc4424fb46
khansadilla <117127484+khansadilla@users.noreply.github.com>
2026-04-07 10:44:37 +0700

    change to openai

--- llm/model.py
+++ llm/model.py
@@ -1,11 +1,16 @@
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 
-llm = ChatGroq(
+llm = ChatOpenAI(
+    model="gpt-4o-mini",  # murah tapi bagus
+    temperature=0.7
+)
+'''llm = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.7,
     frequency_penalty=0.6,
     request_timeout=20
-)
+)'''
 
 
 llm_judge = ChatGroq(
